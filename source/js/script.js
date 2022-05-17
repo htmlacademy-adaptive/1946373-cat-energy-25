9e01118becdcd9b3c1d196902683a739f0490ccc
Алиса <alicecarry64@gmail.com>
2022-05-18 02:46:03 +0300

    добавляет стили

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,15 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-header__toggle');
 
-console.log(navMain)
-navMain.classList.remove('main-nav--nojs');
 
-console.log(navToggle)
+navMain.classList.remove('main-nav--nojs');
 navToggle.classList.remove('main-header__toggle--nojs');
+navMain.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', function () {
   navMain.classList.toggle('main-nav--closed');
 
-
-  navToggle.classList.toggle('main-header__toggle--closed');
   navToggle.classList.toggle('main-header__toggle--opened');
-
+  navToggle.classList.toggle('main-header__toggle--closed');
 });
 
